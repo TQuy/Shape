7037964752c7f3b5d8cf20efd2d3a21912fb387a
quynt <quynguyenthe000@gmail.com>
2022-07-05 09:15:09 +0700

    update test

--- manageshape/tests.py
+++ manageshape/tests.py
@@ -20,11 +20,18 @@ class ShapeTestCase(TestCase):
         for i in range(len(shape_list)):
             triangle = Shape.objects.create(name=shape_list[i], type=shape_list[i], user=user)
         self.assertEqual(Shape.objects.count(), len(shape_list))
+
         # check unique contraint
         with self.assertRaises(IntegrityError) as err:
             with transaction.atomic():
-                Shape.objects.create(name=shape_list[i], type=shape_list[i], user=user)
+                Shape.objects.create(name=shape_list[0], type=shape_list[0], user=user)
         self.assertTrue(re.match(r"unique", str(err.exception), re.IGNORECASE))
+
+        # check update_or_create
+        new_shape = Shape.objects.get(user=user, name=shape_list[0])
+        new_shape.type = shape_list[1]
+        new_shape.save()
+
         # check CASCADE delete
         user.delete()
         self.assertEqual(Shape.objects.count(), 0)
